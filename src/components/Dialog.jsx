d0a3b40143e86137eb8941785609c5eb62107ce1
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-08-03 14:05:03 -0300

    update: folder location

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -2,7 +2,7 @@
 
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { Cross2Icon } from '@radix-ui/react-icons'
-import { cn } from '../../utils'
+import { cn } from '../utils'
 import { forwardRef } from 'react'
 
 const Dialog = DialogPrimitive.Root
