baedead368769f0cc37d4cdc82d164293c912500
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-08-04 11:11:57 -0300

    update: removed stagger

--- src/pages/Home/animation/useAnimation.js
+++ src/pages/Home/animation/useAnimation.js
@@ -11,14 +11,13 @@ export function useAnimation() {
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
       gsap.fromTo(
-        animation.current.children,
-        { opacity: 0, scale: 0.9 },
+        animation.current,
+        { opacity: 0, scale: 1.1 },
         {
           opacity: 1,
           scale: 1,
           delay: 0.25,
           duration: 1,
-          stagger: 0.1,
           ease: 'power3.expo4',
         }
       )
