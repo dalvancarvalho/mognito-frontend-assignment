13995c8f0c5ae4861b4a9eab62f016ce4b144037
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-08-04 23:21:37 -0300

    removed unused packages and components

--- src/utils/index.jsx
+++
@@ -1,9 +0,0 @@
-/* Utils/index.jsx */
-
-import { clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs) {
-  // Helper function used by Dialog component
-  return twMerge(clsx(inputs))
-}
